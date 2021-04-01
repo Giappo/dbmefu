334b90fd00c97a9dd5634025441d7334e4d5a53c
Giappo <glaudanno@gmail.com>
2021-04-02 00:35:25 +0200

    find elements of db1 not in db2

--- R/in_db2_not_in_db1.R
+++ R/in_db2_not_in_db1.R
@@ -15,3 +15,21 @@ in_db2_not_in_db1 <- function(
   }
   df3
 }
+
+#' @export
+in_db1_not_in_db2 <- function(
+  df1,
+  df2,
+  choose_folder = FALSE
+) {
+  nomi1 <- df1["Nome"][[1]]
+  nomi2 <- df2["Nome"][[1]]
+
+  df3 <- df2[(!nomi1 %in% nomi2), ]
+  if (choose_folder) {
+    write.csv(df3, file = file.path(utils::choose.dir(), "not_in_db2.csv"))
+  } else {
+    write.csv(df3, file = "not_in_db2.csv")
+  }
+  df3
+}
